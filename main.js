170d8bed992367e2e3f0b820b3c1706d13256a4f
Mariano Todeschini <94144548+marianotodeschini@users.noreply.github.com>
2023-08-15 21:25:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,223 +2,306 @@ const informacionCategorias = {
    oficios : [
     {
         nombre: "Cocinero",
-        descripcion: `Ahora eres ${nombre} + Un experto en preparar deliciosos platillos.`
+        descripcion: ` Un experto en preparar deliciosos platillos.`
     },
     {
         nombre: "Asador",
-        descripcion: `Ahora eres ${nombre} + Un maestro en cocinar a la parrilla.`
+        descripcion: ` Un maestro en cocinar a la parrilla.`
     },
     {
         nombre: "Cantante",
-        descripcion: `Ahora eres ${nombre} + Un artista que emociona a todos con su voz.`
+        descripcion: ` Un artista que emociona a todos con su voz.`
     },
     {
         nombre: "Carpintero",
-        descripcion: `Ahora eres ${nombre} + Un hábil constructor de muebles y estructuras.`
+        descripcion: `Un hábil constructor de muebles y estructuras.`
     },
     {
         nombre: "Pianista",
-        descripcion: `Ahora eres ${nombre} + Un virtuoso del piano que encanta a todos con su música.`
+        descripcion: `Un virtuoso del piano que encanta a todos con su música.`
     },
     {
         nombre: "Vocalista",
-        descripcion: `Ahora eres ${nombre} + Un cantante talentoso que lidera bandas y llena estadios.`
+        descripcion: `Un cantante talentoso que lidera bandas y llena estadios.`
     },
     {
         nombre: "Escritor",
-        descripcion: `Ahora eres ${nombre} + Un autor creativo que transporta a los lectores a otros mundos con sus historias.`
+        descripcion: `Un autor creativo que transporta a los lectores a otros mundos con sus historias.`
     },
     {
         nombre: "Diputado",
-        descripcion: `Ahora eres ${nombre} + Un representante del pueblo que trabaja en la legislación y toma decisiones importantes.`
+        descripcion: ` Un representante del pueblo que trabaja en la legislación y toma decisiones importantes.`
     },
     {
         nombre: "Relaciones Públicas",
-        descripcion: `Ahora eres ${nombre} + Un experto en construir y mantener la imagen positiva de empresas y figuras públicas.`
+        descripcion: `Un experto en construir y mantener la imagen positiva de empresas y figuras públicas.`
     },
     {
         nombre: "Urólogo",
-        descripcion: `Ahora eres ${nombre} + Un médico especializado en el sistema urinario y problemas relacionados.`
+        descripcion: `Un médico especializado en el sistema urinario y problemas relacionados.`
     }
 ],
 personajes :[
     {
         nombre: "Susana Giménez",
-        descripcion: `Ahora eres ${nombre} + Una icónica presentadora y figura de la televisión argentina.`
+        descripcion: ` Una icónica presentadora y figura de la televisión argentina.`
     },
     {
         nombre: "Lali Espósito",
-        descripcion: `Ahora eres ${nombre} + Una talentosa actriz y cantante conocida por su energía en el escenario.`
+        descripcion: ` Una talentosa actriz y cantante conocida por su energía en el escenario.`
     },
     {
         nombre: "Cristina Kirchner",
-        descripcion: `Ahora eres ${nombre} + Una política influyente que ha ocupado cargos importantes en Argentina.`
+        descripcion: ` Una política influyente que ha ocupado cargos importantes en Argentina.`
     },
     {
         nombre: "RuPaul",
-        descripcion: `Ahora eres ${nombre} + Una famosa drag queen y presentadora de televisión.`
+        descripcion: ` Una famosa drag queen y presentadora de televisión.`
     },
     {
         nombre: "Oriana Juncos",
-        descripcion: `Ahora eres ${nombre} + Una destacada deportista que ha sobresalido en su disciplina.`
+        descripcion: `Una destacada deportista que ha sobresalido en su disciplina.`
     },
     {
         nombre: "Marcelo Tinelli",
-        descripcion: `Ahora eres ${nombre} + Un reconocido productor y conductor de programas de entretenimiento.`
+        descripcion: ` Un reconocido productor y conductor de programas de entretenimiento.`
     },
     {
         nombre: "Shrek",
-        descripcion: `Ahora eres ${nombre} + Un ogro querido por su personalidad única en el cine animado.`
+        descripcion: `Un ogro querido por su personalidad única en el cine animado.`
     },
     {
         nombre: "Shakira",
-        descripcion: `Ahora eres ${nombre} + Una cantante y bailarina colombiana de fama internacional.`
+        descripcion: ` Una cantante y bailarina colombiana de fama internacional.`
     },
     {
         nombre: "Harry Potter",
-        descripcion: `Ahora eres ${nombre} + Un joven mago que ha cautivado a millones con sus aventuras mágicas.`
+        descripcion: ` Un joven mago que ha cautivado a millones con sus aventuras mágicas.`
     },
     {
         nombre: "Betty La Fea",
-        descripcion: `Ahora eres ${nombre} + Un personaje peculiar que ha dejado una huella en la cultura popular por su historia de transformación.`
+        descripcion: ` Un personaje peculiar que ha dejado una huella en la cultura popular por su historia de transformación.`
     }
 ],
 objetos : [
     {
         nombre: "Micrófono",
-        descripcion: `Ahora eres ${nombre} + Un dispositivo que amplifica tu voz y te permite hablar ante una audiencia.`
+        descripcion: ` Un dispositivo que amplifica tu voz y te permite hablar ante una audiencia.`
     },
     {
         nombre: "Pala",
-        descripcion: `Ahora eres ${nombre} + Una herramienta utilizada para cavar y mover tierra u otros materiales.`
+        descripcion: ` Una herramienta utilizada para cavar y mover tierra u otros materiales.`
     },
     {
         nombre: "Microondas",
-        descripcion: `Ahora eres ${nombre} + Un electrodoméstico que utiliza microondas para calentar y cocinar alimentos rápidamente.`
+        descripcion: ` Un electrodoméstico que utiliza microondas para calentar y cocinar alimentos rápidamente.`
     },
     {
         nombre: "Toalla",
-        descripcion: `Ahora eres ${nombre} + Un pedazo de tela que se utiliza para secar el cuerpo después de bañarse.`
+        descripcion: ` Un pedazo de tela que se utiliza para secar el cuerpo después de bañarse.`
     },
     {
         nombre: "Planetas",
-        descripcion: `Ahora eres ${nombre} + Cuerpos celestes que orbitan alrededor de una estrella, como la Tierra, Marte, Júpiter, etc.`
+        descripcion: ` Cuerpos celestes que orbitan alrededor de una estrella, como la Tierra, Marte, Júpiter, etc.`
     },
     {
         nombre: "OVNIs",
-        descripcion: `Ahora eres ${nombre} + Objetos Voladores No Identificados, que a menudo se asocian con fenómenos inexplicables en el cielo.`
+        descripcion: ` Objetos Voladores No Identificados, que a menudo se asocian con fenómenos inexplicables en el cielo.`
     },
     {
         nombre: "Dinero",
-        descripcion: `Ahora eres ${nombre} + Medio de intercambio y medida de valor utilizado en transacciones comerciales.`
+        descripcion: ` Medio de intercambio y medida de valor utilizado en transacciones comerciales.`
     },
     {
         nombre: "Saumerio",
-        descripcion: `Ahora eres ${nombre} + Una mezcla de hierbas aromáticas que se quema para crear fragancias y purificar el aire.`
+        descripcion: ` Una mezcla de hierbas aromáticas que se quema para crear fragancias y purificar el aire.`
     },
     {
         nombre: "Cubiertos",
-        descripcion: `Ahora eres ${nombre} + Utensilios de cocina utilizados para comer, como cuchillos, tenedores y cucharas.`
+        descripcion: `Utensilios de cocina utilizados para comer, como cuchillos, tenedores y cucharas.`
     },
     {
         nombre: "Flan",
-        descripcion: `Ahora eres ${nombre} + Un postre suave y delicioso hecho con huevos, leche y azúcar caramelizado.`
+        descripcion: ` Un postre suave y delicioso hecho con huevos, leche y azúcar caramelizado.`
     }
 ],
 cantantes : [
     {
         nombre: "Karol G",
-        descripcion: `Ahora eres ${nombre} + Una exitosa cantante de música urbana con millones de seguidores.`
+        descripcion: `A Una exitosa cantante de música urbana con millones de seguidores.`
     },
     {
         nombre: "David Bisbal",
-        descripcion: `Ahora eres ${nombre} + Un talentoso cantante español conocido por su voz única.`
+        descripcion: ` Un talentoso cantante español conocido por su voz única.`
     },
     {
         nombre: "Celia Cruz",
-        descripcion: `Ahora eres ${nombre} + Una legendaria cantante cubana de música latina. ¡Azúcar!`
+        descripcion: ` Una legendaria cantante cubana de música latina. ¡Azúcar!`
     },
     {
         nombre: "Britney Spears",
-        descripcion: `Ahora eres ${nombre} + Una icónica artista pop que marcó una generación con su música y estilo.`
+        descripcion: ` Una icónica artista pop que marcó una generación con su música y estilo.`
     },
     {
         nombre: "Miley Cyrus",
-        descripcion: `Ahora eres ${nombre} + Una versátil cantante y actriz que ha evolucionado en su carrera artística.`
+        descripcion: ` Una versátil cantante y actriz que ha evolucionado en su carrera artística.`
     },
     {
         nombre: "Christina Aguilera",
-        descripcion: `Ahora eres ${nombre} + Una poderosa voz en el mundo del pop y el R&B.`
+        descripcion: ` Una poderosa voz en el mundo del pop y el R&B.`
     },
     {
         nombre: "Babasónicos",
-        descripcion: `Ahora eres ${nombre} + Una influyente banda de rock alternativo de Argentina.`
+        descripcion: ` Una influyente banda de rock alternativo de Argentina.`
     },
     {
         nombre: "Maria Becerra",
-        descripcion: `Ahora eres ${nombre} + Una joven y talentosa cantante que ha ganado popularidad en la música urbana.`
+        descripcion: ` Una joven y talentosa cantante que ha ganado popularidad en la música urbana.`
     },
     {
         nombre: "Coldplay",
-        descripcion: `Ahora eres ${nombre} + Una banda británica conocida por sus emotivas canciones y espectáculos en vivo.`
+        descripcion: `+ Una banda británica conocida por sus emotivas canciones y espectáculos en vivo.`
     },
     {
         nombre: "Elegante",
-        descripcion: `Ahora eres ${nombre} + Un artista con un estilo único y una propuesta musical fresca.`
+        descripcion: ` Un artista con un estilo único y una propuesta musical fresca.`
     }
 ],
 lugares : [
     {
         nombre: "Río de Janeiro",
-        descripcion: `Ahora estás en ${nombre} + Una ciudad vibrante en Brasil conocida por sus playas y celebraciones festivas.`
+        descripcion: ` Una ciudad vibrante en Brasil conocida por sus playas y celebraciones festivas.`
     },
     {
         nombre: "Ámsterdam",
-        descripcion: `Ahora estás en ${nombre} + La capital de los Países Bajos, famosa por sus canales y su rica historia.`
+        descripcion: ` La capital de los Países Bajos, famosa por sus canales y su rica historia.`
     },
     {
         nombre: "Londres",
-        descripcion: `Ahora estás en ${nombre} + La capital del Reino Unido, una metrópolis cultural y cosmopolita.`
+        descripcion: `La capital del Reino Unido, una metrópolis cultural y cosmopolita.`
     },
     {
         nombre: "Liniers",
-        descripcion: `Ahora estás en ${nombre} + Un barrio en la ciudad de Buenos Aires, Argentina, con su propia identidad y encanto.`
+        descripcion: ` Un barrio en la ciudad de Buenos Aires, Argentina, con su propia identidad y encanto.`
     },
     {
         nombre: "Avellaneda",
-        descripcion: `Ahora estás en ${nombre} + Una ciudad en Argentina, famosa por su historia industrial y su pasión futbolera.`
+        descripcion: ` Una ciudad en Argentina, famosa por su historia industrial y su pasión futbolera.`
     },
     {
         nombre: "Barcelona",
-        descripcion: `Ahora estás en ${nombre} + Una ciudad costera en España, conocida por su arquitectura única y ambiente animado.`
+        descripcion: `Una ciudad costera en España, conocida por su arquitectura única y ambiente animado.`
     },
     {
         nombre: "Mykonos",
-        descripcion: `Ahora estás en ${nombre} + Una isla griega famosa por sus playas hermosas y su vida nocturna.`
+        descripcion: ` Una isla griega famosa por sus playas hermosas y su vida nocturna.`
     },
     {
         nombre: "Dubái",
-        descripcion: `Ahora estás en ${nombre} + Una ciudad en los Emiratos Árabes Unidos, conocida por su lujo y arquitectura moderna.`
+        descripcion: ` Una ciudad en los Emiratos Árabes Unidos, conocida por su lujo y arquitectura moderna.`
     },
     {
         nombre: "Puerto Madero",
-        descripcion: `Ahora estás en ${nombre} + Un distrito en Buenos Aires, Argentina, conocido por su arquitectura y vida nocturna.`
+        descripcion: ` Un distrito en Buenos Aires, Argentina, conocido por su arquitectura y vida nocturna.`
     },
     {
         nombre: "Palermo",
-        descripcion: `Ahora estás en ${nombre} + Un barrio en Buenos Aires, Argentina, famoso por su diversidad cultural y opciones de entretenimiento.`
+        descripcion: ` Un barrio en Buenos Aires, Argentina, famoso por su diversidad cultural y opciones de entretenimiento.`
     },
     {
         nombre: "Italia",
-        descripcion: `Ahora estás en ${nombre} + Un país europeo conocido por su rica historia, arte y deliciosa gastronomía.`
+        descripcion: ` Un país europeo conocido por su rica historia, arte y deliciosa gastronomía.`
     },
     {
         nombre: "México",
-        descripcion: `Ahora estás en ${nombre} + Un país en América del Norte, famoso por su cultura, tradiciones y hermosos paisajes.`
+        descripcion: ` Un país en América del Norte, famoso por su cultura, tradiciones y hermosos paisajes.`
     },
     {
         nombre: "Nueva York",
-        descripcion: `Ahora estás en ${nombre} + Una ciudad icónica en los Estados Unidos, llena de rascacielos, diversidad y energía constante.`
+        descripcion: ` Una ciudad icónica en los Estados Unidos, llena de rascacielos, diversidad y energía constante.`
     }
 ]
 };
+
+ const jugadores = [];
+function agregarDato() {
+
+   
+
+    const inputElement = document.getElementById('nuevoJugador');
+    const dato = inputElement.value.trim();
+   
+    
+    if (dato !== '') {
+        const $listaElement = document.getElementById('listaJugador');
+        const nuevoDato = document.createElement('li');
+        
+        nuevoDato.textContent = dato;
+        nuevoDato.addEventListener('click', function() {
+            $listaElement.removeChild(this);
+            jugadores.splice(jugadores.indexOf(dato), 1);
+        });
+        
+        $listaElement.appendChild(nuevoDato);
+    } 
+ 
+    jugadores.push(dato);
+    inputElement.value = "";
+};
+
+
+
+
+;
+ 
+
+// Llamar a la función para asignar categorías (aquí se pasa la categoría seleccionada)
+;
+
+function guardarCategoria() {
+    
+   const  categoriaElemento = document.getElementById('categoria');
+   const valorCategoria = categoriaElemento.textContent;
+   const categoriaSeleccionada = valorCategoria;
+  console.log(categoriaSeleccionada);
+}
+
+
+
+
+const contadorElement = document.getElementById('contador');
+const mensajeElement = document.getElementById('mensaje');
+
+let contador = 3;
+
+const interval = setInterval(() => {
+    contador--;
+
+    if (contador > 0) {
+        contadorElement.textContent = contador;
+    } else {
+        clearInterval(interval);
+        mensajeElement.style.display = 'block';
+    }
+}, 1000); // Intervalo de 1 segundo (1000 ms)
+
+
+
+const contenedorPrincipal = document.getElementsByClassName('contenedor-principal')[0];
+const nuevoDiv = document.createElement('div');
+contenedorPrincipal.appendChild(nuevoDiv);
+
+const mostrarRol = `<h1> Eres</h1>
+<h3> ${informacionCategorias.cantantes[4].nombre}</h3>
+<p> ${informacionCategorias.cantantes[4].descripcion}</p>
+<button onclick="siguienteJugador()">Siguiente Jugador</button>
+`;
+
+const agregarElemento = setInterval(() =>{
+    mensajeElement.style.display = 'none';
+    nuevoDiv.innerHTML = mostrarRol
+
+}, 4000);
+;
+
+
